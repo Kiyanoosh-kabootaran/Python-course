ccb23699f67bdd0218ec0b92a6a7e031e0a9d6a8
Kiyanoosh <kabootaran1384@gmail.com>
2026-02-09 15:31:23 +0330

    alarm clock project

---
+++ 63-alarm-clock.py
@@ -0,0 +1,21 @@
+# Python Alarm Clock
+import time
+import datetime
+
+def set_alarm(alarm_time):
+  print(f"Alarm set for {alarm_time}")
+  
+  is_running = True
+
+  while is_running:
+    current_time = datetime.datetime.now().strftime("%H:%M:%S")
+    print(current_time)
+    time.sleep(1)
+
+    if current_time == alarm_time:
+      print("Wake Up! 😵")
+      is_running = False
+
+if __name__ == '__main__':
+  alarm_time = input("Enter the alarm time (HH:MM:SS) : ")
+  set_alarm(alarm_time)
